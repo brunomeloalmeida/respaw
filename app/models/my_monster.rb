c058c1ab77f159077526cdbe0b8bbcb7aad05d38
brunomeloalmeida <bruno_melo_almeida@hotmail.com>
2016-08-03 16:43:43 -0300

    Nem me lembro quais foram as alteracoes v10

--- app/models/my_monster.rb
+++ app/models/my_monster.rb
@@ -1,4 +1,37 @@
 class MyMonster < ActiveRecord::Base
   belongs_to :monster
   belongs_to :summoner
+
+  def status_rune_hp
+	if self.hp_rune.blank? 
+			0
+	else
+		self.hp_rune
+	end
+  end
+
+  def status_rune_atk
+	if self.atk_rune.blank? 
+			0
+	else
+		self.atk_rune
+	end
+  end
+
+  def status_rune_def
+	if self.def_rune.blank? 
+			0
+	else
+		self.def_rune
+	end
+  end
+
+  def status_rune_vel
+	if self.vel_rune.blank? 
+			0
+	else
+		self.vel_rune
+	end
+  end
+
 end
